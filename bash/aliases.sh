7f148136c1bd8fd3c19a9195cacbbfa128a602d1
Henrik N <henrik@nyh.se>
2012-12-14 17:07:37 +0100

    Some Rails testing aliases.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -3,3 +3,7 @@ alias gs="git status"
 alas gw="git show"
 alias gd="git diff"
 alias gpp="git pull --rebase && git push"
+
+# Rails
+alias rsu="rake spec:unit"       # Unit tests.
+alias rsua="rake spec:unit:all"  # Unit tests including Rails unit tests.
